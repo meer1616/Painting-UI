37792767216caa0314fa2371e252eecb1ae750f8
Meer <meerpatel.1616@gmail.com>
2021-05-22 12:50:26 +0530

    removing useless

--- js/resp.js
+++ js/resp.js
@@ -1,24 +1,14 @@
-function toogle(){
-        console.log("clicked")
-
-let horiznavbar=document.getElementById("horiznavbar");
-let ham=document.getElementById("ham");
-let close=document.getElementById("close");
-
-
-if(horiznavbar.style.display=="block"){
-        horiznavbar.style.display="none";
-        close.style.display="none";
-        ham.style.display="block";
-
-// horiznavbar.style.transition="5s";
-}
-else{
-        horiznavbar.style.display="block"
-        ham.style.display="none";
-        close.style.display="block";
-
-        // horiznavbar.style.transition="5s";
-
-}
+function toogle() {
+  let horiznavbar = document.getElementById("horiznavbar");
+  let ham = document.getElementById("ham");
+  let close = document.getElementById("close");
+  if (horiznavbar.style.display == "block") {
+    horiznavbar.style.display = "none";
+    close.style.display = "none";
+    ham.style.display = "block";
+  } else {
+    horiznavbar.style.display = "block";
+    ham.style.display = "none";
+    close.style.display = "block";
+  }
 }
